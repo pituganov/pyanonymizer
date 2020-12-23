f5948868797f0b8bc5619810ae5300d8be2d6da0
Станислав Питуганов <pitugano@intersvyaz.net>
2020-12-24 00:22:59 +0500

    feat: вынес парамеры в cli

--- pyanonymizer/__main__.py
+++ pyanonymizer/__main__.py
@@ -10,14 +10,12 @@ from deeppavlov import build_model
 ROOT_DIR = Path(__file__).resolve().parents[1]
 
 
-def main(filename: Path, savepath: Path, column: int):
+def main(filename: Path, savepath: Path, column: int, encoding: str, sep: str):
     """main"""
     preprocessor = build_model(f"{ROOT_DIR}/full_preprocessing.json")
 
     if "csv" in filename.suffix:
-        data = pd.read_csv(
-            filename, header=None, sep=";", encoding="windows-1251"
-        )
+        data = pd.read_csv(filename, header=None, sep=sep, encoding=encoding)
     else:
         data = pd.read_excel(filename, header=None)
 
@@ -42,5 +40,12 @@ if __name__ == "__main__":
         help="Номер столбца, который нужно анонимизировать",
         type=int,
     )
+    parser.add_argument("--sep", help="Символ-разделитель", type=str)
+
+    parser.add_argument(
+        "--encoding", "-e", help="Кодировка", type=str, default="windows-1251"
+    )
     args = parser.parse_args()
-    main(args.filename, args.savepath, args.column - 1)
+    main(
+        args.filename, args.savepath, args.column - 1, args.encoding, args.sep
+    )
